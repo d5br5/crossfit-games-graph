ca15d5482a888be2223d66badff5ca1dd1a2752d
DohyungKim <ehgud456456@naver.com>
2024-03-05 00:41:42 +0900

    [Feat] full navbar

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -5,15 +5,13 @@ import ThemeSwitch from "@/src/layouts/theme/Switch";
 
 const Header = () => {
   return (
-    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-      <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-        <Link href="/">
-          <div className="font-semibold text-lg">Crossfit Games Graph</div>
-        </Link>
-        <div className="flex gap-2">
-          <AuthButton />
-          <ThemeSwitch />
-        </div>
+    <nav className="w-full flex justify-between items-center text-sm border-b border-b-foreground/10 h-16 py-3 px-5">
+      <Link href="/">
+        <div className="font-semibold text-lg">Crossfit Games Graph</div>
+      </Link>
+      <div className="flex gap-2">
+        <AuthButton />
+        <ThemeSwitch />
       </div>
     </nav>
   );
