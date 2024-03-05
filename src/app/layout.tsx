7f1a8c13d9f3384bddb5cf331efda83074194d97
Doh Kim <ehgud456456@naver.com>
2024-03-05 10:00:20 +0900

    [Update] metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Crossfit Games Graph",
+  description: "The leaderboard distribution graph for Crossfit Games",
 };
 
 export default function RootLayout({
