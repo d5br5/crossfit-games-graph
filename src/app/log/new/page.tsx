f2d7b5ffcd80735775d2d7fae03a93dd1f13ecda
Doh Kim <ehgud456456@naver.com>
2024-03-19 12:57:40 +0900

    [Init] install calendar, datepicker

--- src/app/log/new/page.tsx
+++ src/app/log/new/page.tsx
@@ -1,20 +1,57 @@
 "use client";
 
 import { Button } from "@/src/components/ui/button";
+import { useState } from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/src/components/ui/calendar";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/src/components/ui/popover";
 
 const NewLogPage = () => {
+  const [date, setDate] = useState<Date>();
+
   const onSubmit = () => {
     console.log("submit");
+    console.log(date);
   };
 
   return (
     <div className="max-w-[1200px] w-full">
       <h1>새 운동일지</h1>
       <div className="flex flex-col gap-3">
-        <div className="border rounded-md flex py-3 px-6">
-          <div>날짜</div>
-          <div>장소</div>
-        </div>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-[280px] justify-start text-left font-normal",
+                !date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date ? format(date, "PPP") : <span>Pick a date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0 items-center flex flex-col pb-3">
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              initialFocus
+            />
+            <PopoverClose asChild>
+              <Button size="sm">날짜 선택 및 닫기</Button>
+            </PopoverClose>
+          </PopoverContent>
+        </Popover>
+        <div>장소</div>
         <div className="border rounded-md py-3 px-6">내용</div>
       </div>
       <div className="w-full text-right mt-4">
