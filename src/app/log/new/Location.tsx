8b4716a468e62e2f8b355f2174e9a9c3b75d016f
Doh Kim <ehgud456456@naver.com>
2024-03-20 13:54:53 +0900

    [Refactor] componentize LocationGroup

--- src/app/log/new/Location.tsx
+++ src/app/log/new/Location.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Check, MapPinned } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { Button } from "@/src/components/ui/button";
+import { Check, MapPinned } from "lucide-react";
 
 import {
   Popover,
@@ -15,7 +16,6 @@ import {
   CommandItem,
   CommandList,
 } from "@/src/components/ui/command";
-import { Button } from "@/src/components/ui/button";
 
 interface Props {
   serverData: string[];
@@ -25,6 +25,11 @@ interface Props {
   setLocation: React.Dispatch<React.SetStateAction<string>>;
 }
 
+interface CommandProps {
+  heading: string;
+  group: string[];
+}
+
 export default function Location({
   serverData,
   clientData,
@@ -32,21 +37,57 @@ export default function Location({
   location,
   setLocation,
 }: Props) {
+  const [open, setOpen] = React.useState(false);
   const [inputValue, setInputValue] = React.useState("");
-  const [locationOpen, setLocationOpen] = React.useState(false);
-  const [locationHoverValue, setLocationHoverValue] = React.useState("");
+  const [hoverValue, setHoverValue] = React.useState("");
 
-  const isPrevLocation = [...serverData, ...clientData].some(
+  const isLocationNameExists = [...serverData, ...clientData].some(
     (item) => item === inputValue
   );
 
+  const handleSelectItem = (value: string) => {
+    setLocation(value);
+    setOpen(false);
+  };
+
+  const createNewLocation = () => {
+    setOpen(false);
+    setClientData((prev) => [...prev, inputValue]);
+    setLocation(inputValue);
+  };
+
+  const LocationGroup = ({ heading, group }: CommandProps) => {
+    return (
+      <CommandGroup heading={heading}>
+        {group.map((item) => {
+          const isSelected = item === location;
+          const className = cn(
+            "mr-2 h-4 w-4",
+            isSelected ? "opacity-100" : "opacity-0"
+          );
+          return (
+            <CommandItem
+              key={item}
+              value={item}
+              className={cn(isSelected && "bg-accent text-accent-foreground")}
+              onSelect={handleSelectItem}
+            >
+              <Check className={className} />
+              {item}
+            </CommandItem>
+          );
+        })}
+      </CommandGroup>
+    );
+  };
+
   return (
     <Popover
-      open={locationOpen}
-      onOpenChange={(value) => {
+      open={open}
+      onOpenChange={(open) => {
         // open 상태를 업데이트하고, input 및 hover value를 초기화
-        setLocationOpen(value);
-        setLocationHoverValue(location);
+        setOpen(open);
+        setHoverValue(location);
         setInputValue("");
       }}
     >
@@ -54,20 +95,15 @@ export default function Location({
         <Button
           variant="outline"
           role="combobox"
-          aria-expanded={locationOpen}
+          aria-expanded={open}
           className={cn("w-[280px] justify-between")}
         >
-          {location
-            ? [...clientData, ...serverData].find((item) => item === location)
-            : "장소"}
+          {location || "장소"}
           <MapPinned className="ml-2 size-4 shrink-0" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[280px] p-0">
-        <Command
-          value={locationHoverValue}
-          onValueChange={setLocationHoverValue}
-        >
+        <Command value={hoverValue} onValueChange={setHoverValue}>
           <CommandList>
             <CommandInput
               placeholder="장소 검색 / 신규 등록"
@@ -75,71 +111,13 @@ export default function Location({
               onValueChange={setInputValue}
             />
 
-            <CommandGroup heading="내 장소">
-              {serverData.map((item) => {
-                const isSelected = item === location;
-                return (
-                  <CommandItem
-                    key={item}
-                    value={item}
-                    className={cn(
-                      isSelected && "bg-accent text-accent-foreground"
-                    )}
-                    onSelect={(currentValue) => {
-                      setLocation(currentValue);
-                      setLocationHoverValue(currentValue);
-                      setLocationOpen(false);
-                    }}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        isSelected ? "opacity-100" : "opacity-0"
-                      )}
-                    />
-                    {item}
-                  </CommandItem>
-                );
-              })}
-            </CommandGroup>
-            <CommandGroup heading="새로 추가된 장소">
-              {clientData.map((item) => {
-                const isSelected = item === location;
-                return (
-                  <CommandItem
-                    key={item}
-                    value={item}
-                    className={cn(
-                      isSelected && "bg-accent text-accent-foreground"
-                    )}
-                    onSelect={(currentValue) => {
-                      setLocation(currentValue);
-                      setLocationHoverValue(currentValue);
-                      setLocationOpen(false);
-                    }}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 size-4",
-                        isSelected ? "opacity-100" : "opacity-0"
-                      )}
-                    />
-                    {item}
-                  </CommandItem>
-                );
-              })}
-            </CommandGroup>
+            <LocationGroup heading="내 장소" group={serverData} />
+            <LocationGroup heading="새로 추가된 장소" group={clientData} />
             {inputValue !== "" && (
               <CommandGroup>
                 <CommandItem
-                  disabled={isPrevLocation}
-                  onSelect={() => {
-                    setLocationOpen(false);
-                    setClientData((prev) => [...prev, inputValue]);
-                    setLocation(inputValue);
-                    setLocationHoverValue(inputValue);
-                    setInputValue("");
-                  }}
+                  disabled={isLocationNameExists}
+                  onSelect={createNewLocation}
                 >
                   신규 장소 추가 : {inputValue}
                 </CommandItem>
